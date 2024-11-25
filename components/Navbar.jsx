dbbabae5c7fbc49e7e772afcf11103a3528bd655
sparsh013 <oltikarsparsh013@gmail.com>
2024-11-25 20:01:20 +0530

    Updated files with new changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="flex flex-col md:flex-row items-center justify-between bg-gray-800 text-white shadow-lg p-6 mb-6 rounded-lg">
       <h1 className="text-4xl font-bold hover:text-indigo-400 transition duration-300 ease-in-out">
-        <Link href="/">DxO Wiki</Link>
+        <Link href="/">Sparsh's Wiki</Link>
       </h1>
       <div className="mt-4 md:mt-0">
         <Search />
